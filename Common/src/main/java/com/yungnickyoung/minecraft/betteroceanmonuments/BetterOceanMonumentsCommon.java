2804b743a4251a4e21cac3dec5e1699878f6368b
Nick Young <yungnickyoung@gmail.com>
2022-10-17 17:08:08 -0400

    v1.0.3. Switch to manual AutoReg package scanning. Dep YAPI 2.2.3.

--- Common/src/main/java/com/yungnickyoung/minecraft/betteroceanmonuments/BetterOceanMonumentsCommon.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/betteroceanmonuments/BetterOceanMonumentsCommon.java
@@ -2,6 +2,7 @@ package com.yungnickyoung.minecraft.betteroceanmonuments;
 
 import com.yungnickyoung.minecraft.betteroceanmonuments.module.ConfigModule;
 import com.yungnickyoung.minecraft.betteroceanmonuments.services.Services;
+import com.yungnickyoung.minecraft.yungsapi.api.YungAutoRegister;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,6 +14,7 @@ public class BetterOceanMonumentsCommon {
     public static final ConfigModule CONFIG = new ConfigModule();
 
     public static void init() {
+        YungAutoRegister.scanPackageForAnnotations("com.yungnickyoung.minecraft.betteroceanmonuments.module");
         Services.MODULES.loadModules();
     }
 }
